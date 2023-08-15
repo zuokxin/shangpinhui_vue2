f85d1476ca6a6e9ee46badcd818dfaf89a7aeaf9
fuyou-data <1639318926@qq.com>
2023-08-16 01:47:57 +0800

    初始化

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,7 +3,7 @@
         <Header></Header>
         <!-- 路由·组件出口 -->
         <router-view></router-view>
-        <Footer></Footer>
+        <Footer v-show="$route.meta.show"></Footer>
     </div>
 </template>
 
@@ -18,4 +18,4 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped></style>
